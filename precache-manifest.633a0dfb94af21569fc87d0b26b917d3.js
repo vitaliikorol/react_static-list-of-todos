a543f7f08ecc970a1612c89c18754072891819c3
Vitalii Korol <vitaliikorol@github.com>
2020-04-23 23:45:51 +0300

    Updates

---
+++ precache-manifest.633a0dfb94af21569fc87d0b26b917d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d0d187bcb31a7fbb88455b44b93a77",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8455b236feb767c80e83",
+    "url": "./static/css/main.72e0f8d2.chunk.css"
+  },
+  {
+    "revision": "cdbfa4b0ed9a17660ba3",
+    "url": "./static/js/2.b0f90b1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b0f90b1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8455b236feb767c80e83",
+    "url": "./static/js/main.c9bdfee2.chunk.js"
+  },
+  {
+    "revision": "dc13ecc3e0fecfe0c39b",
+    "url": "./static/js/runtime-main.33c19533.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.633a0dfb94af21569fc87d0b26b917d3.js"
 );
 
 self.addEventListener('message', (event) => {
